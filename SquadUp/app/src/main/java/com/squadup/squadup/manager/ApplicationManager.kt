fa3eedb82d3f86f96236f7113b2c764bef781fd8
Jason Corriveau <jpc028@bucknell.edu>
2017-11-17 10:58:11 -0500

    Setting up application manager

--- SquadUp/app/src/main/java/com/squadup/squadup/manager/ApplicationManager.kt
+++ SquadUp/app/src/main/java/com/squadup/squadup/manager/ApplicationManager.kt
@@ -1,6 +1,7 @@
 package com.squadup.squadup.manager
 
 import android.app.Application
+import com.squadup.squadup.data.User
 
 /**
  * A manager class for handling global data throughout the app.
@@ -9,6 +10,8 @@ class ApplicationManager : Application() {
 
     // The manager that handles Google App Engine calls and HTTP requests.
     lateinit var backend: BackendManager
+
+    var user: User? = null
 
     // Sets up all the initial data for the application.
     fun setup() {
